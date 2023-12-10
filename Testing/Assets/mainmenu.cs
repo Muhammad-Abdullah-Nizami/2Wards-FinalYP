32721d734de4d5045ede3bbc53dbd69a5ae18de4
AimanHayat <135201882+SaadnAiman@users.noreply.github.com>
2023-12-11 01:00:43 +0500

    created pause menu
    press esc for pause

--- Testing/Assets/mainmenu.cs
+++ Testing/Assets/mainmenu.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainmenu : MonoBehaviour
 {
     //ayman
-    //creating scriptfor ui   
+    //creating scriptfor ui
+    //
+
+
+    //variable for pause menu
+    public static bool GameIspaused = false;
+    public GameObject PauseMenuUI;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIspaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
     public void playgame()
     {
         Invoke("Loadgame", 0.1f);
@@ -29,4 +50,31 @@ public class mainmenu : MonoBehaviour
     {
         SceneManager.LoadScene("SampleScene");
     }
+    //function for pause menu
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIspaused = false;
+    }
+    //function for pause menu
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIspaused= true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("LOADING GAME...");
+    }
+
+    //public void QuitGame()
+    //{
+    //    Debug.Log("QUITING GAME...");
+    //    //Application.Quit();
+    //}
 }
